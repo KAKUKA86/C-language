4321f32ff6d82972bba2f421c6fee1ad037b9061
KAKUKA86 <zhangbang2002@foxmail.com>
2021-01-12 18:42:28 +0800

    练习摸鱼

---
+++ 1009.C
@@ -0,0 +1,27 @@
+#include<stdio.h>
+/*
+Description
+Your task is to Calculate a + b.
+Input
+The input will consist of a series of pairs of integers a and b, separated by a space, one pair of integers per line.
+Output
+For each pair of input integers a and b you should output the sum of a and b, and followed by a blank line.
+Sample Input
+1 5
+10 20
+Sample Output
+6
+
+30
+*/
+int main(void)
+{
+	int a = 0, b = 0, sum;
+	while(scanf("%d%d", &a, &b) != EOF)
+	{
+		sum = a + b;
+		printf("%d\n", sum);
+		printf("\n");
+	}
+	return 0;
+}

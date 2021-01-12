4d6a38c41cade2cb8a8f615ffd484f839118a57f
张邦宸 <73161183+KAKUKA86@users.noreply.github.com>
2021-01-12 17:51:12 +0800

    Update 1008.C

--- 1008.C
+++ 1008.C
@@ -1,4 +1,18 @@
 #include <stdio.h>
+/*
+Description
+Your task is to calculate the sum of some integers.
+Input
+Input contains multiple test cases, and one case one line. Each case starts with an integer N, and then N integers follow in the same line.
+Output
+For each test case you should output the sum of N integers in one line, and with one line of output for each line in input.
+Sample Input
+4 1 2 3 4
+5 1 2 3 4 5
+Sample Output
+10
+15
+*/
 int main(void)
 {
 	int N, i, sum = 0, u;
